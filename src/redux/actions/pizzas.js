a82220dcb1abec115e4668a67b02d05c159cfb12
Kirill Artimenia <kirill.artimenya@mail.ru>
2020-07-30 20:07:35 +0300

    add

---
+++ src/redux/actions/pizzas.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export const setLoaded = (payload) => ({
+  type: 'SET_LOADED',
+  payload,
+});
+
+export const fetchPizzas = (sortBy, category) => (dispatch) => {
+  dispatch({
+    type: 'SET_LOADED',
+    payload: false,
+  });
+
+  axios
+    .get(
+      "http://localhost:3001/pizzas"
+    )
+    .then(({ data }) => {
+      dispatch(setPizzas(data));
+    });
+};
+
+export const setPizzas = (items) => ({
+  type: 'SET_PIZZAS',
+  payload: items,
+});
